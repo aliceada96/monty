59d56039ae91ff562a1cdbefe25cc603d774cb62
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-22 12:44:45 +0300

    main file

---
+++ monty_main.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: the array of args
+ * 
+ * Return: Exit success if succesful, else exit fail 
+ * 
+*/
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char *filename;
+
+	(void) argv;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+
+	filename = argv[1];
+	file = fopen(filename, "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file '%s'\n", filename);
+		return (EXIT_FAILURE);
+	}
+
+	exec_(file);
+
+	fclose(file);
+
+	return (EXIT_SUCCESS);
+}
